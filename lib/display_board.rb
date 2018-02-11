09c05734c57350550b3cae5dc4cf7f109146722d
Angela A <ranran.wang@wework.com>
2018-02-11 20:57:38 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,6 +4,7 @@
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
 board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2] "
   puts "-----------"  
