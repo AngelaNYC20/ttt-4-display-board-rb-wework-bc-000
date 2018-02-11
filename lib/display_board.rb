3978daf95f6a41979276d0ccf36bfcd1e3c7dfc8
Angela A <ranran.wang@wework.com>
2018-02-11 20:59:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,6 +4,7 @@
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
 board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2] "
